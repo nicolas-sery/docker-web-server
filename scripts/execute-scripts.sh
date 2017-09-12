4974860fa848781f5187dae9b06b72b81685c921
nicolas-sery <nicolas.sery@gmail.com>
2017-09-12 20:27:21 +0200

    project initialisation

---
+++ scripts/execute-scripts.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#starting services
+bash /tmp/scripts/start-services.sh
+
+#keeping container alive
+bash /tmp/scripts/keep-alive.sh 
---
+++ scripts/start-services.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+service ssh start
+service apache2 start
